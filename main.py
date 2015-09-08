2a59671aeee68372b32b1a9f13d1cd07dcb4311b
smorante <smorante@ing.uc3m.es>
2015-09-08 12:44:51 +0200

    trying zscores

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ import Superpixels
 from ClothContour import ClothContour
 
 ########################################################
+
+ALPHA=0.5
+
 ########################################################
 
 def get_coloured_item(image):
@@ -91,8 +94,8 @@ def normalize_1Channel_image(image):
     min_value = masked_depth_image[np.unravel_index(np.where(masked_depth_image == 0, 1000,masked_depth_image).argmin(), masked_depth_image.shape)]
     max_value = masked_depth_image[np.unravel_index(np.where(masked_depth_image == 1000, 0,masked_depth_image).argmax(), masked_depth_image.shape)]
     range_value = max_value-min_value
-    print "Scaled depth image dimensions: " + str(scaled_depth_map.shape)
-    print "Depth image range: (%d, %d) delta=%d" % (min_value, max_value, range_value)
+#    print "Scaled depth image dimensions: " + str(scaled_depth_map.shape)
+#    print "Depth image range: (%d, %d) delta=%d" % (min_value, max_value, range_value)
     scaled_depth_map = np.where(scaled_depth_map != 1000, (scaled_depth_map - min_value) * (255/range_value), 255)
     scaled_depth_map = scaled_depth_map.astype(np.uint8)
 #    plt.figure(2)
@@ -101,13 +104,35 @@ def normalize_1Channel_image(image):
     return scaled_depth_map
     
 def calculateAdequacy(profile, selection):
-    
+    var=0
+    print "len:", len(profile)
     if selection==1:
+        
+        for i in range(1,len(profile)):
+#            print "profile elems: ", profile[i], "  ", profile[i-1]
+#            print "difference:", np.abs(int(profile[i])-int(profile[i-1]))
+#            
+#            # cast to int becasue they were unit8 and failed to substract them
+            var+=np.abs(int(profile[i])-int(profile[i-1]))
+#            print "var:", var
+
+        return var
+ 
+    if selection==2:
         return np.sum(profile)
+  
+    if selection==3:
+        return np.sum(profile)
+
+    if selection==4:
+        return np.sum(profile)
+  
     else:
         return False
     
-    
+def get_outlier(points, thresh=2):
+    results = stats.zscore(points) 
+    return results.flatten() > thresh, results
 #####################################################################
 #####################################################################
 #####################################################################
@@ -117,7 +142,7 @@ image_paths, depth_maps = load_data('./data/20150625_single')
 for path_rgb, path_depth in zip(image_paths, depth_maps):
     # Load image
     image = cv2.imread(path_rgb)
-    print "Loaded rgb image, dimensions: " + str(image.shape)
+#    print "Loaded rgb image, dimensions: " + str(image.shape)
 
     # Get mask
     mask = get_coloured_item(image)
@@ -128,7 +153,7 @@ for path_rgb, path_depth in zip(image_paths, depth_maps):
     # Load depth map
     depth_image = np.loadtxt(path_depth)
     masked_depth_image = np.where(mask==255, depth_image.transpose(), 1000)
-    print "Loaded depth image, dimensions: " + str(depth_image.shape)
+#    print "Loaded depth image, dimensions: " + str(depth_image.shape)
 
     # Normalize depth map
     scaled_depth_map = normalize_1Channel_image(masked_depth_image)
@@ -194,7 +219,6 @@ for path_rgb, path_depth in zip(image_paths, depth_maps):
             start = [p for p in path[0]]
             end = [p for p in path[1]]
             path_samples_avg = Superpixels.line_sampling(avg, start , end, 1)
-            profiles.append(path_samples_avg)            
             points = Superpixels.line_sampling_points(start, end, 1)
 
             fig, ax = plt.subplots(1, 2)
@@ -203,6 +227,8 @@ for path_rgb, path_depth in zip(image_paths, depth_maps):
 
             ax[0].set_title(str(id)+': sampled profiles')
             without_white = [p for p in path_samples_avg if p != 255]
+            profiles.append(without_white)            
+
             ax[0].bar(range(len(without_white)), without_white, 1)
 
             ax[1].set_title(str(id)+': sampling points')
@@ -210,9 +236,18 @@ for path_rgb, path_depth in zip(image_paths, depth_maps):
             ax[1].plot(points[0], points[1], 'b-')
 
 
-###### SELECTING BEST DIRECTION
-    indexes = []
-    for elem in profiles:
-        indexes.append(calculateAdequacy(elem,1))
+###### SELECTING BEST DIRECTIONS
+    adequacy = []
 
-    print "Indexes:", indexes
+    for elem in profiles:
+        adequacy.append(calculateAdequacy(elem,1))
+   
+    print "adequacy:", adequacy
+
+    boolzscores, zscores = get_outlier(adequacy, thresh=ALPHA)
+    print "[INFO] Detected outliers: ", boolzscores, zscores
+
+    fig, ax = plt.figure()
+
+        
+
