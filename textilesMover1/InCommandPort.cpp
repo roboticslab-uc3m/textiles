19f67cf1498371d63e4667b1aef161b3de2885d5
jgvictores <jcgvicto@gmail.com>
2015-09-10 11:59:32 +0200

    fix and comment px to mm transf

--- textilesMover1/InCommandPort.cpp
+++ textilesMover1/InCommandPort.cpp
@@ -13,15 +13,15 @@ void InCommandPort::onRead(Bottle& b) {
         Bottle* cvb = inCvPortPtr->read();
         int pxX1 = cvb->get(0).asInt();
         int pxY1 = cvb->get(1).asInt();
-        double mmZ1 = cvb->get(2).asDouble();
+        double mmZ1 = cvb->get(2).asDouble();  // Points forward.
         int pxX2 = cvb->get(3).asInt();
         int pxY2 = cvb->get(4).asInt();
-        double mmZ2 = cvb->get(5).asDouble();
+        double mmZ2 = cvb->get(5).asDouble();  // Points forward.
 
-        double mmX1 = 1000.0 * ( (pxX1 - DEFAULT_CX_D) * mmZ1/1000.0 ) / DEFAULT_FX_D;
-        double mmX2 = 1000.0 * ( (pxX2 - DEFAULT_CX_D) * mmZ2/1000.0 ) / DEFAULT_FX_D;
-        double mmY1 = 1000.0 * ( (pxY1 - DEFAULT_CY_D) * mmZ1/1000.0 ) / DEFAULT_FY_D;
-        double mmY2 = 1000.0 * ( (pxY2 - DEFAULT_CY_D) * mmZ2/1000.0 ) / DEFAULT_FY_D;
+        double mmX1 = -(1000.0 * ( (pxX1 - DEFAULT_CX_D) * mmZ1/1000.0 ) / DEFAULT_FX_D);  // Points right thanks to change sign so (x ^ y = z). Expects --noMirror.
+        double mmX2 = -(1000.0 * ( (pxX2 - DEFAULT_CX_D) * mmZ2/1000.0 ) / DEFAULT_FX_D);  // Points right thanks to change sign so (x ^ y = z). Expects --noMirror.
+        double mmY1 = 1000.0 * ( (pxY1 - DEFAULT_CY_D) * mmZ1/1000.0 ) / DEFAULT_FY_D;  // Points down.
+        double mmY2 = 1000.0 * ( (pxY2 - DEFAULT_CY_D) * mmZ2/1000.0 ) / DEFAULT_FY_D;  // Points down.
 
     }
 }
