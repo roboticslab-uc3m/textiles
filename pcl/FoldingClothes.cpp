af559820181ea7fb56a81196a4c138573f8db756
David-Estevez <david.estevez.fdez@gmail.com>
2015-11-11 19:51:53 +0100

    Add code for plane fitting with ransac

--- pcl/FoldingClothes.cpp
+++ pcl/FoldingClothes.cpp
@@ -6,9 +6,15 @@
 #include <pcl/console/parse.h>
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
+//-- Normals
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/filter.h>
+//-- Plane fitting
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 void show_usage(char * program_name)
 {
@@ -48,7 +54,7 @@ int main(int argc, char* argv[])
     }
 
     //-- Load point cloud data
-    pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
     if (file_is_pcd)
     {
@@ -73,7 +79,7 @@ int main(int argc, char* argv[])
     //-- Stuff goes on here
     //-- Find normals
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimator;
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
 
     normalEstimator.setInputCloud(source_cloud);
@@ -86,15 +92,53 @@ int main(int argc, char* argv[])
     pcl::removeNaNNormalsFromPointCloud(*cloud_normals, *filtered_normals, new_ordering);
     std::cout << "After NaN removal: " << filtered_normals->size() << " normals." << std::endl;
 
+    //-- Plane fitting
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::SACSegmentation<pcl::PointXYZ> segmentation;
+    segmentation.setOptimizeCoefficients(true);
+    segmentation.setModelType(pcl::SACMODEL_PLANE);
+    segmentation.setMethodType(pcl::SAC_RANSAC);
+    segmentation.setDistanceThreshold(5);
+    segmentation.setInputCloud(source_cloud);
+    segmentation.segment(*inliers, *coefficients);
+
+    if (inliers->indices.size() == 0)
+    {
+        std::cerr << "Could not estimate a planar model for input point cloud." << std::endl;
+    }
+    else
+    {
+        std::cout << "Model coefficients: " << coefficients->values[0] << " "
+                                            << coefficients->values[1] << " "
+                                            << coefficients->values[2] << " "
+                                            << coefficients->values[3] << std::endl;
+        std::cout << "Model inliers: " << inliers->indices.size() << std::endl;
+    }
+
     //-- Visualization Setup
     pcl::visualization::PCLVisualizer viewer("Folding clothes");
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_color_handler(source_cloud, 255, 0, 0);
-    viewer.addPointCloud(source_cloud, cloud_color_handler, "loaded_point_cloud");
-    viewer.addPointCloudNormals<pcl::PointXYZ, pcl::Normal> (source_cloud, cloud_normals, 1, 0.8, "normals");
     viewer.addCoordinateSystem(1.0, "cloud", 0);
     viewer.setBackgroundColor(0.05, 0.05, 0.05, 0);
+
+    //-- Add point cloud
+    viewer.addPointCloud(source_cloud, cloud_color_handler, "loaded_point_cloud");
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "loaded_point_cloud");
+
+    //-- Add plane inliers
+    pcl::PointCloud<pcl::PointXYZ>::Ptr plane_points(new pcl::PointCloud<pcl::PointXYZ>(*source_cloud, inliers->indices));
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> plane_color_handler(plane_points, 0, 255, 0);
+    viewer.addPointCloud(plane_points, plane_color_handler, "plane_point_cloud");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "plane_point_cloud");
+
+    //-- Add normals
+    viewer.addPointCloudNormals<pcl::PointXYZ, pcl::Normal> (source_cloud, cloud_normals, 1, 0.8, "normals");
     viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0, 0, 255, "normals");
+
+    //-- Add a plane
+    //viewer.addPlane(*coefficients,"ransac_plane");
+    //viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0, 255, 0, "ransac_plane");
 
     //-- Visualization thread
     while(!viewer.wasStopped())
