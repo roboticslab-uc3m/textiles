432e68eb950cec35c8a861c30e2b27a3890b7724
David-Estevez <david.estevez.fdez@gmail.com>
2015-12-09 16:44:08 +0100

    Try watershed segmentation on projected garment image

--- textiles/load_pcl_data.py
+++ textiles/load_pcl_data.py
@@ -5,8 +5,10 @@ import pylab
 from skimage.filters.rank import median
 from skimage.morphology import disk
 
+import GarmentAnalysis
+
 if __name__ == '__main__':
-    src = np.loadtxt('../pcl/build/depth_image.m')
+    src = np.loadtxt('../pcl/build/depth_image2.m')
     print np.unique(src).shape
     filtered_src = median(src, disk(5))
     pylab.figure(0)
@@ -14,3 +16,8 @@ if __name__ == '__main__':
     pylab.figure(1)
     pylab.imshow(filtered_src)
     pylab.show()
+
+    ga = GarmentAnalysis.GarmentAnalysis()
+    ga.compute(filtered_src)
+
+    print "Found %d pathes" % ga.n_patches
