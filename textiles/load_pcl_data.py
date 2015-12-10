e5aac3d1d3e7e0f8a7d33e73d708508850930b2d
David-Estevez <david.estevez.fdez@gmail.com>
2015-12-10 15:26:16 +0100

    Add code to plot 3d point cloud with curvature data

--- textiles/load_pcl_data.py
+++ textiles/load_pcl_data.py
@@ -7,6 +7,31 @@ from skimage.morphology import disk
 
 import GarmentAnalysis
 
+def surface_plot(data):
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X = data[:,0]
+    Y = data[:,1]
+    # X, Y = np.meshgrid(X, Y)
+    Z = data[:,2]
+    G = data[:,4]
+    N = G/G.max()  # normalize 0..1
+    ax.xaxis.set_ticks(np.arange(-1, 1, 0.1))
+    ax.yaxis.set_ticks(np.arange(-1, 1, 0.1))
+    ax.set_autoscalex_on(False)
+    ax.set_autoscaley_on(False)
+    ax.scatter(X,Y,Z, c=N, cmap=plt.cm.spectral)
+
+    # ax.plot(X, Y, Z, 'o', cmap=cm.jet(N))
+    # surf = ax.plot_surface(
+    #    X, Y, Z, rstride=1, cstride=1,
+    #    facecolors=cm.jet(N),
+    #    linewidth=0, antialiased=False, shade=False)
+    plt.show()
+
 if __name__ == '__main__':
     src = np.loadtxt('../pcl/build/depth_image2.m')
     print np.unique(src).shape
@@ -15,9 +40,11 @@ if __name__ == '__main__':
     pylab.imshow(src)
     pylab.figure(1)
     pylab.imshow(filtered_src)
-    pylab.show()
+    # pylab.show()
 
-    ga = GarmentAnalysis.GarmentAnalysis()
-    ga.compute(filtered_src)
+    # ga = GarmentAnalysis.GarmentAnalysis()
+    # ga.compute(filtered_src)
+    # print "Found %d pathes" % ga.n_patches
 
-    print "Found %d pathes" % ga.n_patches
+    data = np.loadtxt('../pcl/build/rsd_data.m')
+    surface_plot(data)
