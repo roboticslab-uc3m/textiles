ce8438bb1732c6450c3991dc9f0e74a974caafa3
David-Estevez <david.estevez.fdez@gmail.com>
2015-12-25 21:21:56 +0100

    Add support for color in renderer

--- pcl/render2png.cpp
+++ pcl/render2png.cpp
@@ -1,3 +1,11 @@
+/*******************************************************
+ *  render2png
+ * ----------------------------------------------------
+ *
+ * Obtain a 2D representation of whatever the viewer is
+ * outputting in a png file
+ *
+ * *****************************************************/
 #include <vector>
 #include <iostream>
 #include <sstream>
@@ -10,10 +18,10 @@
 
 void show_usage(char * program_name)
 {
-  std::cout << std::endl;
-  std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
-  std::cout << "-h:  Show this help." << std::endl;
-  std::cout << "-o:  Output file PNG" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+    std::cout << "-h:  Show this help." << std::endl;
+    std::cout << "-o:  Output file PNG" << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -33,72 +41,58 @@ int main(int argc, char* argv[])
 
     //-- Get point cloud file from arguments
     std::vector<int> filenames;
-    bool file_is_pcd = false;
 
-    filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
+    filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
 
     if (filenames.size() < 1)
     {
-        filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
-
-        if (filenames.size() < 1)
-        {
-            show_usage(argv[0]);
-            return -1;
-        }
-        else
-        {
-            file_is_pcd = true;
-        }
-    }
-
-    //-- Load all files
-    pcl::PointCloud<pcl::PointXYZ>::Ptr clouds[filenames.size()];
-    for (int i = 0; i < filenames.size(); i++)
-    {
-        std::string current_file = argv[filenames[i]];
-        clouds[i] = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
-
-        std::cout << "Loading: " << current_file << std::endl;
-
-        if (file_is_pcd)
-        {
-            if (pcl::io::loadPCDFile(current_file.c_str(), *clouds[i]) < 0)
-            {
-                std::cout << "Error loading point cloud " << current_file.c_str() << std::endl << std::endl;
-                show_usage(argv[0]);
-                return -1;
-            }
-        }
-        else
-        {
-            if (pcl::io::loadPLYFile(current_file.c_str(), *clouds[i]) < 0)
-            {
-                std::cout << "Error loading point cloud " << current_file.c_str() << std::endl << std::endl;
-                show_usage(argv[0]);
-                return -1;
-            }
-        }
+        show_usage(argv[0]);
+        return -1;
     }
 
     //-- Visualization Setup
     pcl::visualization::PCLVisualizer viewer("render2png");
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red_color_handler(clouds[0], 255, 0, 0);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> green_color_handler(clouds[0], 0, 255, 0);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> blue_color_handler(clouds[0], 0, 0, 255);
-    viewer.addCoordinateSystem(1.0, "cloud", 0);
+    viewer.addCoordinateSystem(0.5, "cloud", 0);
     viewer.setBackgroundColor(0.05, 0.05, 0.05, 0);
     viewer.setShowFPS(false);
     viewer.setCameraPosition(-0.277017, -1.53399, 1.72656,  0.176012, 0.717815, 0.673618);
 
+    //-- Add point clouds
+    pcl::PCDReader pcd;
+    pcl::PCLPointCloud2::Ptr cloud;
+    pcl::visualization::PointCloudColorHandler<pcl::PCLPointCloud2>::Ptr color_handler;
 
-    //-- Add point cloud
-    for (int i = 0; i < filenames.size(); i++)
+    for (size_t i = 0; i < filenames.size(); i++)
     {
-        std::stringstream ss;
-        ss << "cloud" << i;
-        viewer.addPointCloud(clouds[i], red_color_handler, ss.str().c_str());
-        viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, ss.str().c_str());
+        //-- Load point cloud
+        cloud.reset(new pcl::PCLPointCloud2);
+        Eigen::Vector4f origin;
+        Eigen::Quaternionf orientation;
+        int version;
+        std::cout << "Loading: " << argv[filenames[i]] << std::endl;
+        if (pcd.read(argv[filenames[i]], *cloud, origin, orientation, version) < 0)
+        {
+            std::cerr << "Error ocurred, exiting..." << std::endl;
+            return -1;
+        }
+
+        //-- Select cloud color or random color
+        color_handler.reset(new pcl::visualization::PointCloudColorHandlerRandom<pcl::PCLPointCloud2>(cloud));
+        for(size_t j = 0; j < cloud->fields.size(); j++)
+        {
+            if (cloud->fields[j].name == "rgb" || cloud->fields[j].name == "rgba")
+            {
+                color_handler.reset(new pcl::visualization::PointCloudColorHandlerRGBField<pcl::PCLPointCloud2>(cloud));
+                break;
+            }
+        }
+
+
+        //-- Add point cloud
+        std::stringstream cloud_name;
+        cloud_name << argv[filenames[i]] << "-" << i;
+        viewer.addPointCloud(cloud, color_handler, origin, orientation, cloud_name.str().c_str());
+        viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, cloud_name.str().c_str());
     }
 
     //-- Generate render and save
