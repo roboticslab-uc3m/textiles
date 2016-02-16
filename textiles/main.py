eccee59021b4e247fef990220cb9e46f87061119
David-Estevez <david.estevez.fdez@gmail.com>
2016-02-16 12:46:54 +0100

    Add refactoring stuff

--- textiles/main.py
+++ textiles/main.py
@@ -1,6 +1,22 @@
+__author__ = "def"
+
+import cv2
+import numpy as np
+
 from GarmentSegmentation import GarmentSegmentation
 from GarmentDepthMapAnalysis import GarmentDepthMapAnalysis
 from GarmentPickAndPlacePoints import GarmentPickAndPlacePoints
+from utils import load_data
 
 if __name__ == "__main__":
-    pass
+
+    image_paths, depth_image_paths = load_data('../data/20150902')
+
+    for path_rgb_image, path_depth_image in zip(image_paths, depth_image_paths):
+        # Load input data
+        image_src = cv2.imread(path_rgb_image)
+        depth_image = np.loadtxt(path_depth_image)
+
+        # Garment Segmentation Step
+        mask = GarmentSegmentation.background_substraction()
+        approx = GarmentSegmentation.calculate_approx_polygon()
