c50d378a5ba0caf7b475ed46eacd20eec2c9223d
David-Estevez <david.estevez.fdez@gmail.com>
2016-02-22 13:17:44 +0100

    Add plot functions for each stage

--- textiles/GarmentPlot.py
+++ textiles/GarmentPlot.py
@@ -54,3 +54,57 @@ def plot_pick_and_place_points(image_src, pick_point, place_point, show=True):
                head_width=15, head_length=15, fc='blue', ec='blue', lw=5)
     if show:
         plt.show()
+
+def plot_segmentation_stage(image_rgb, mask, polygon, to_file=None):
+    plt.figure()
+    plt.imshow(image_rgb)
+    plt.imshow(mask, cmap=plt.cm.gray, alpha=0.7)
+    points = [tuple(point[0]) for point in polygon]
+    # Plot lines
+    for (start_x, start_y), (end_x, end_y) in zip(points, points[1:]+points[0:1]):
+        plt.plot( (start_x, end_x), (start_y, end_y), 'r-', linewidth=2.0, alpha=0.7 )
+    # Plot points
+    for x, y in points:
+        plt.plot(x, y, 'ro', alpha=0.7)
+    plt.axis('off')
+
+    if to_file:
+        pass
+    else:
+        plt.show()
+
+def plot_clustering_stage(image_rgb, labeled_image, to_file=None):
+    plt.figure()
+    plt.imshow(image_rgb)
+    plt.imshow(labeled_image, cmap=plt.cm.RdGy, alpha=0.6)
+    plt.axis('off')
+
+    if to_file:
+        pass
+    else:
+        plt.show()
+
+def plot_pick_and_place_stage(image_rgb, labeled_image, approximated_polygon, unfold_paths,
+                              pick_point, place_point, to_file=None):
+    plot_rgb(image_rgb, show=False)
+    plt.imshow(labeled_image, cmap=plt.cm.RdGy, alpha=0.6)
+    points = [tuple(point[0]) for point in approximated_polygon]
+    # Plot polygon lines
+    for (start_x, start_y), (end_x, end_y) in zip(points, points[1:]+points[0:1]):
+        plt.plot( (start_x, end_x), (start_y, end_y), 'r-', linewidth=2.0 )
+    # Plot polygon points
+    for x, y in points:
+        plt.plot(x, y, 'ro')
+    #Plot unfold paths
+    for path in unfold_paths:
+        (start_x, start_y), (end_x, end_y) = path
+        plt.plot( (start_x, end_x), (start_y, end_y), 'go-', linewidth=2.0 )
+    # Plot arrow
+    plt.arrow(pick_point[0], pick_point[1], place_point[0]-pick_point[0], place_point[1]-pick_point[1],
+               head_width=15, head_length=15, fc='blue', ec='blue', lw=5, alpha=0.7)
+    plt.axis('off')
+
+    if to_file:
+        pass
+    else:
+        plt.show()
