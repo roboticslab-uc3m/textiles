1739db801a7f628d06227f6fb65eee48c95986ea
David-Estevez <david.estevez.fdez@gmail.com>
2016-03-07 14:50:58 +0100

    Add support to read images from yarp

--- textiles/yarp_utils.py
+++ textiles/yarp_utils.py
@@ -9,7 +9,7 @@ class YarpCamera(object):
     """
     def __init__(self, port, resolution):
         self.port = port
-        self.resolution = resolution
+        self.width, self.height = resolution
 
     @abstractmethod
     def get_image(self):
@@ -23,17 +23,47 @@ class YarpRGBCamera(YarpCamera):
         super(YarpRGBCamera, self).__init__(port, resolution)
 
     def get_image(self):
-        pass
+        # Create image placeholder
+        img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+
+        # Read image with yarp
+        yarp_img = yarp.ImageRgb()
+        yarp_img.resize(self.width, self.height)
+        yarp_img.setExternal(img, self.width, self.height)
+        if not self.port.read(yarp_img):
+            raise ValueError("Try to read a closed port")
+
+        # Check image
+        if not yarp_img.getRawImage().__long__() == img.__array_interface__['data'][0]:
+            raise RuntimeError("read() reallocated my yarp_img")
+
+        return img
+
 
 class YarpDepthCamera(YarpCamera):
     # def __init__(self, port, resolution):
     #     super(YarpDepthCamera, self).__init(port, resolution)
 
     def get_image(self):
-        pass
+        # Create image placeholder
+        img = np.zeros((self.height, self.width), dtype=np.uint16)
+
+        # Read image with yarp
+        yarp_img = yarp.ImageMono16()
+        yarp_img.resize(self.width, self.height)
+        yarp_img.setExternal(img, self.width, self.height)
+        if not self.port.read(yarp_img):
+            raise ValueError("Try to read a closed port")
+
+        # Check image
+        if not yarp_img.getRawImage().__long__() == img.__array_interface__['data'][0]:
+            raise RuntimeError("read() reallocated my yarp_img")
+
+        return img
 
 class YarpNetworkException(RuntimeError):
     """ Exception raised when some error ocurred in the yarp network (exception body explains the error) """
+
 
 @contextlib.contextmanager
 def get_cameras(rgb_camera, rgb_resolution, depth_camera, depth_resolution):
@@ -47,7 +77,7 @@ def get_cameras(rgb_camera, rgb_resolution, depth_camera, depth_resolution):
     rgb_camera_port.open("/python/rgb:i")
     if not yarp.Network.connect(rgb_camera, "/python/rgb:i"):
         raise YarpNetworkException("Could not connect to RGB camera stream")
-    rgb = YarpRGBCamera(rgb_camera, rgb_resolution)
+    rgb = YarpRGBCamera(rgb_camera_port, rgb_resolution)
 
     # Connect to depth camera
     depth_camera_port = yarp.Port()
@@ -69,23 +99,21 @@ if __name__ == '__main__':
     # Small example of how to use this file
     import matplotlib.pyplot as plt
 
-    with get_cameras("/OpenNI2/imageFrame:o", (640, 420),
-                     "/OpenNI2/depthFrame:o", (640, 420)) as (rgb_camera, depth_camera):
+    with get_cameras("/OpenNI2/imageFrame:o", (640, 480),
+                     "/OpenNI2/depthFrame:o", (640, 480)) as (rgb_camera, depth_camera):
         rgb_image = rgb_camera.get_image()
         depth_image = depth_camera.get_image()
 
-        if rgb_image and depth_image:
-            plt.figure()
-            plt.title("RGB image")
-            plt.imshow(rgb_image)
+        plt.figure()
+        plt.title("RGB image")
+        plt.imshow(rgb_image)
 
-            plt.figure()
-            plt.imshow("Depth image")
-            plt.imshow(depth_camera, cmap=plt.cm.RdGy)
+        plt.figure()
+        plt.title("Depth image")
+        plt.imshow(depth_image, cmap=plt.cm.RdGy)
 
-            plt.show()
-        else:
-            print "Failed to grab images"
+        plt.show()
+
 
 
 
