db55889356766650b3b77cb84072f38179c62dc8
David-Estevez <david.estevez.fdez@gmail.com>
2016-04-29 16:46:33 +0200

    Refactor Superpixels (kind of)

--- textiles/Superpixels.py
+++ textiles/Superpixels.py
@@ -4,41 +4,6 @@ import operator
 
 __author__ = 'def', 'smorante'
 
-
-def get_masks_slic(slic_image):
-    labels = np.unique(slic_image)
-    masks = []
-
-    for label in labels:
-        # Copy image and do threshold
-        mask = np.where(slic_image == label, 255, 0)
-        masks.append(mask)
-
-    return masks
-
-
-def get_average_regions(image, regions_image):
-    """ Returns an image with the median value for each superpixel """
-    labels = np.unique(regions_image)
-    avg = np.zeros(regions_image.shape, np.uint8)
-
-    for elem in labels:
-        avg_value = np.median(np.extract(regions_image == elem,image))
-        avg = np.where(regions_image == elem, int(avg_value), avg)
-        
-    return avg
-
-def get_average_slic(image, slic_image):
-    """ Returns an image with the median value for each superpixel """
-    labels = np.unique(slic_image)
-    avg = np.zeros(slic_image.shape, np.uint8)
-
-    for label in labels:
-        avg_value = np.median(np.extract(slic_image == label,image))
-        avg = np.where(slic_image == label, int(avg_value), avg)
-
-    return avg
-
 def get_highest_superpixel(image):
     """ Returns the superpixel with the lowest value (highest in the point cloud) """
     labels = np.unique(image)
@@ -55,7 +20,6 @@ def get_highest_point_with_superpixels(image):
 def get_centroid(image):
     m = measure.moments(image)
     return m[0, 1] / m[0, 0], m[1, 0] / m[0, 0]
-
 
 def line_sampling_points(p1, p2, step):
     # Order points:
@@ -90,6 +54,3 @@ def line_sampling(image, p1, p2, step):
         return [ int(image[int(j), int(i), :]) for i, j in zip(x, y)]
     else:
         return [ int(image[int(j), int(i)]) for i, j in zip(x, y)]
-
-
-
