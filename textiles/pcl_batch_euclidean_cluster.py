7063d73971d604b178c61c16d71ba7b98dee76ca
David-Estevez <david.estevez.fdez@gmail.com>
2016-05-31 16:45:16 +0200

    Add option to parellize cluster processing with several processes

--- textiles/pcl_batch_euclidean_cluster.py
+++ textiles/pcl_batch_euclidean_cluster.py
@@ -11,7 +11,9 @@ import subprocess
 import numpy as np
 from pcl_utils import  colorize_point_cloud
 
-garment_data_names = ["Jacket1", "Jacket2", "Jacket3", "Jacket4", "Jacket5"]
+parallelize = True # True to run several processes in parallel
+
+garment_data_names = ["LeatherJacket1", "Jacket2", "Jacket3", "Jacket4", "Jacket5"]
 garment_data_pattern = "~/Research/point_clouds/{0}/mesh_1_cut.ply"
 
 pcl_processing_binary = "clusteringCleanup"
@@ -22,40 +24,57 @@ pcl_renderer_folder = "~/Repositories/textiles/pcl/build"
 
 output_folder = "~/Repositories/textiles/data/3D data"
 
+def process_single_cloud(args):
+    name, debug = args
 
-if __name__ == "__main__":
-    for name in garment_data_names:
+    if debug:
         print "Garment: " + name
 
-        # Create output dir if needed (and empty old data)
-        current_output_folder = os.path.expanduser(os.path.join(output_folder, name))
-        if not os.path.exists(current_output_folder):
-            os.makedirs(current_output_folder)
-        else:
-            for filename in glob.glob(os.path.join(current_output_folder, "*")):
-                try:
-                    os.remove(filename)
-                except OSError, e:
-                    pass
+    # Create output dir if needed (and empty old data)
+    current_output_folder = os.path.expanduser(os.path.join(output_folder, name))
+    if not os.path.exists(current_output_folder):
+        os.makedirs(current_output_folder)
+    else:
+        for filename in glob.glob(os.path.join(current_output_folder, "*")):
+            try:
+                os.remove(filename)
+            except OSError, e:
+                pass
 
-        # Call the processing program:
-        args = [ os.path.expanduser(os.path.join(pcl_processing_folder, pcl_processing_binary)),
-                 "-o",
-                 current_output_folder,
-                 os.path.expanduser(garment_data_pattern.format(name))]
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-
+    # Call the processing program:
+    args = [os.path.expanduser(os.path.join(pcl_processing_folder, pcl_processing_binary)),
+            "-o",
+            current_output_folder,
+            os.path.expanduser(garment_data_pattern.format(name))]
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if debug:
         print "Command: " + str(" ".join(args))
         print "Processing...\n" + str(out)
         print str(err)
-
-        # Take all point clouds and generate a render
-        args = [os.path.expanduser(os.path.join(pcl_renderer_folder, pcl_renderer_binary)),
-                "-o", os.path.join(current_output_folder, "render.png")]
-        args += glob.glob(os.path.join(current_output_folder, "*.pcd"))
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-
+    # Take all point clouds and generate a render
+    args = [os.path.expanduser(os.path.join(pcl_renderer_folder, pcl_renderer_binary)),
+            "-o", os.path.join(current_output_folder, "render.png")]
+    args += glob.glob(os.path.join(current_output_folder, "*.pcd"))
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if debug:
         print "Generate picture from input data...\n" + str(out)
         print str(err)
+
+
+if __name__ == "__main__":
+    if parallelize:
+        from multiprocessing import Pool
+        from itertools import repeat
+
+        from tqdm import tqdm
+
+        p = Pool()
+        it = p.imap_unordered(process_single_cloud, zip(garment_data_names, repeat(False)))
+
+        for result in tqdm(it):
+            pass
+    else:
+        for name in garment_data_names:
+            process_single_cloud(name)
