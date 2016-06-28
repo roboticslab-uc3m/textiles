efaf0b78121404c805b42ef893adddbe8fe9652b
David-Estevez <david.estevez.fdez@gmail.com>
2016-06-28 21:18:08 +0200

    Add skeleton code to ironingClothes

--- ironing/perception/ironingClothes.cpp
+++ ironing/perception/ironingClothes.cpp
@@ -1,1 +1,149 @@
-int main (void){}
+/*
+ * ironingClothes
+ * --------------------------------------
+ *
+ * Placeholder text
+ *
+ */
+
+#include <iostream>
+#include <pcl/console/parse.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+#include "Debug.hpp"
+
+void show_usage(char * program_name)
+{
+    std::cout << std::endl;
+    std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+    std::cout << "-h:  Show this help." << std::endl;
+    std::cout << "--ransac-threshold: Set ransac threshold value (default: 0.02)" << std::endl;
+}
+
+int main (int argc, char** argv)
+{
+    //-- Command-line arguments
+    float ransac_threshold = 0.02;
+
+    //-- Show usage
+    if (pcl::console::find_switch(argc, argv, "-h") || pcl::console::find_switch(argc, argv, "--help"))
+    {
+        show_usage(argv[0]);
+        return 0;
+    }
+
+    if (pcl::console::find_switch(argc, argv, "--ransac-threshold"))
+        pcl::console::parse_argument(argc, argv, "--ransac-threshold", ransac_threshold);
+    else
+    {
+        std::cerr << "RANSAC theshold not specified" << std::endl;
+        show_usage(argv[0]);
+        return -2;
+    }
+
+    //-- Get point cloud file from arguments
+    std::vector<int> filenames;
+    bool file_is_pcd = false;
+
+    filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
+
+    if (filenames.size() != 1)
+    {
+        filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
+
+        if (filenames.size() != 1)
+        {
+            show_usage(argv[0]);
+            return -1;
+        }
+
+        file_is_pcd = true;
+    }
+
+    //-- Load point cloud data
+    pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    if (file_is_pcd)
+    {
+        if (pcl::io::loadPCDFile(argv[filenames[0]], *source_cloud) < 0)
+        {
+            std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+            show_usage(argv[0]);
+            return -1;
+        }
+    }
+    else
+    {
+        if (pcl::io::loadPLYFile(argv[filenames[0]], *source_cloud) < 0)
+        {
+            std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+            show_usage(argv[0]);
+            return -1;
+        }
+    }
+
+    //-- Print arguments to user
+    std::cout << "Selected arguments: " << std::endl
+              << "\tRANSAC threshold: " << ransac_threshold << std::endl;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
+    voxel_grid.setInputCloud(source_cloud);
+    voxel_grid.setLeafSize(0.01f, 0.01f, 0.01f);
+    voxel_grid.filter(*cloud_filtered);
+    std::cout << "PointCloud after filtering has: " << cloud_filtered->points.size ()  << " data points." << std::endl; //*
+
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ> ());
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(ransac_threshold);
+
+    int i=0, nr_points = (int) cloud_filtered->points.size();
+    while (cloud_filtered->points.size() > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud(cloud_filtered);
+        seg.segment(*inliers, *coefficients);
+        if (inliers->indices.size() == 0)
+        {
+            std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
+
+        // Extract the planar inliers from the input cloud
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud(cloud_filtered);
+        extract.setIndices(inliers);
+        extract.setNegative(false);
+
+        // Get the points associated with the planar surface
+        extract.filter(*cloud_plane);
+        std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative(true);
+        extract.filter(*cloud_f);
+        *cloud_filtered = *cloud_f;
+    }
+
+    return 0;
+}
