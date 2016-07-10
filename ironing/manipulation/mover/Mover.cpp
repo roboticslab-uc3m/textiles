d3e467f4c866f077131f09e7b15cec5d33e5be8d
jgvictores <jcgvicto@gmail.com>
2016-07-10 23:27:02 +0200

    [ironing] avoid collision in sim

--- ironing/manipulation/mover/Mover.cpp
+++ ironing/manipulation/mover/Mover.cpp
@@ -127,7 +127,6 @@ bool Mover::configure(yarp::os::ResourceFinder &rf) {
     CD_DEBUG("Move arm to good position out of singularity\n");
     {
         std::vector<double> q(7,0.0);
-        q[0] = 20;
         q[3] = 30;
         qMoveAndWait(q);
     }
