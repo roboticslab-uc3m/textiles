2014729104ace6d22b32c93b2cdecd7e3b8440f3
jgvictores <jcgvicto@gmail.com>
2016-07-11 15:19:30 +0200

    initial vel control on strategyBasicVel

--- ironing/manipulation/mover/Mover.cpp
+++ ironing/manipulation/mover/Mover.cpp
@@ -284,21 +284,26 @@ bool Mover::strategyBasicVel()
     std::vector<double> x;
     iCartesianControl->stat(state,x);
     iCartesianControl->movj(x);
+
     CD_DEBUG("***************DOWN*****************\n");
+    std::vector<double> xdot(6,0.0);
+    xdot[2] = -0.1;
+    bool okMove = iCartesianControl->movv(xdot);
+    if( okMove ) {
+        CD_DEBUG("Begin move arm down.\n");
+    } else {
+        CD_WARNING("Failed to begin move arm down.\n");
+    }
+
     double force = 0;
     while( force > forceThreshold )
     {
         yarp::os::Bottle b;
-        x[2] -= 0.005;
-        bool okMove = iCartesianControl->movj(x);
         rightArmFTSensorPort.read(b);
         force = b.get(2).asDouble();
-        if( okMove ) {
-            CD_DEBUG("Moved arm down, %f\n",force);
-        } else {
-            CD_WARNING("Failed to move arm down, %f\n",force);
-        }
+        CD_DEBUG("Moving arm down, %f\n",b.get(2).asDouble());
     }
+    iCartesianControl->stopControl();
 
     CD_DEBUG("***************ADVANCE*****************\n");
     for(int i=0;i<24;i++)
