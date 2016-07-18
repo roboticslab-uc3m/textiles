fbc0921975f4edd0a4ea097f88f6efc99eea1db3
David-Estevez <david.estevez.fdez@gmail.com>
2016-07-18 15:28:07 +0200

    Add code to plot RSD physical meaning in pcd file.

--- unfolding/perception/textiles/pcl_utils.py
+++ unfolding/perception/textiles/pcl_utils.py
@@ -33,6 +33,9 @@ def surface_plot(data):
     #     linewidth=0, antialiased=False, shade=False)
     plt.show()
 
+def rgb_to_pcl_float(r, g, b):
+    return int(r*255) << 16 | int(g*255) << 8 | int(b*255)
+
 def colorize_point_cloud(xyz_data, color_data, output_file, cmap=pylab.cm.cool):
     X = xyz_data[:,0]
     Y = xyz_data[:,1]
@@ -62,13 +65,54 @@ def colorize_point_cloud(xyz_data, color_data, output_file, cmap=pylab.cm.cool):
 
         # Write data
         for x, y, z, color in zip(X, Y, Z, color_data_rgba):
-            color_packed = int(color[0]*255) << 16 | int(color[1]*255) << 8 | int(color[2]*255)
+            color_packed = rgb_to_pcl_float(*color[0:3])
             f.write("{:f} {:f} {:f} {:f}\n".format(x, y, z, color_packed))
 
+def colorize_rsd_point_cloud(xyz_data, r_min_data, r_max_data, output_file):
+    colors = {"plane":rgb_to_pcl_float(255,0,0),
+              "cylinder":rgb_to_pcl_float(0,0,255),
+              "edge":rgb_to_pcl_float(0,255,0),
+              "sphere":rgb_to_pcl_float(0,255,255),
+              "noise":rgb_to_pcl_float(255,255,255)}
+
+    X = xyz_data[:,0]
+    Y = xyz_data[:,1]
+    Z = xyz_data[:,2]
+
+    with open(output_file, 'w') as f:
+        # Write pcd header
+        f.write(
+        """# PCD v.7 - Point Cloud Data file format
+        VERSION .7
+        FIELDS x y z rgb
+        SIZE 4 4 4 4
+        TYPE F F F F
+        COUNT 1 1 1 1
+        WIDTH {0:d}
+        HEIGHT 1
+        VIEWPOINT 0 0 0 1 0 0 0
+        POINTS {0:d}
+        DATA ascii""".format(len(r_min_data)-1))
+
+        # Write data
+        for x, y, z, r_min, r_max in zip(X, Y, Z, r_min_data, r_max_data):
+            # Decision tree:
+            if r_min >0.1:
+                color = colors["plane"]
+            elif r_max > 0.15:
+                color = colors["cylinder"]
+            elif r_max > r_min + 0.05:
+                color = colors["edge"]
+            else:
+                color = colors["sphere"]
+
+            f.write("{:f} {:f} {:f} {:f}\n".format(x, y, z, color))
 
 if __name__ == '__main__':
-    data = np.loadtxt('../pcl/build/curvature_data.m')
+    # data = np.loadtxt('../pcl/build/curvature_data.m')
+    data = np.loadtxt('../../../build/ironing/perception/rsd_wrinkle.m')
     # surface_plot(data)
 
     colorize_point_cloud(data[:,0:3], data[:,3], 'cloud-r_min.pcd')
     colorize_point_cloud(data[:,0:3], data[:,4], 'cloud-r_max.pcd')
+    colorize_rsd_point_cloud(data[:,0:3], data[:,3], data[:,4], 'color-rsd.pcd')
