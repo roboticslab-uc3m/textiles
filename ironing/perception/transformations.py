c991375ab229e6be908e9497c2de4eeccdead158
David-Estevez <david.estevez.fdez@gmail.com>
2016-07-26 19:07:51 +0200

    Add code to send trajectory over yarp

--- ironing/perception/transformations.py
+++ ironing/perception/transformations.py
@@ -8,6 +8,12 @@ class TrajectoryTransform:
         self.H_kinfu_cam = None
         self.pixel_resolution = 0.05
 
+        # Robot transform:
+        self.H_root_cam = np.array([[0.306186,  -0.458636,  0.834208, 0.087413],
+                                    [-0.918559, -0.372462,  0.132372, 0.080342],
+                                    [ 0.250000, -0.806799, -0.535327, 0.686337],
+                                    [ 0.000000,  0.000000,  0.000000, 1.000000]])
+
     def load_from_files(self, H_image_garment_file, H_garment_board_file, H_board_kinfu_file, H_kinfu_cam_file):
         self.H_garment_board = np.loadtxt(H_garment_board_file)
         self.H_board_kinfu = np.loadtxt(H_board_kinfu_file)
@@ -62,7 +68,11 @@ class TrajectoryTransform:
         # Apply transformation to all points
         return [ np.dot(H_cam_image, point) for point in trajectory_image ]
 
-def points_to_file(output_file):
+    def relative_to_robot_root(self, trajectory_image_px):
+        points = self.__call__(trajectory_image_px)
+        return [np.dot(self.H_root_cam, point) for point in points]
+
+def points_to_file(points, output_file):
     with open(output_file, 'w') as f:
         # Write pcd header
         f.write(
@@ -77,9 +87,9 @@ def points_to_file(output_file):
     VIEWPOINT 0 0 0 1 0 0 0
     POINTS {0:d}
     DATA ascii
-    """.format(len(trajectory_debug)))
+    """.format(len(points)))
 
-        for point in trajectory_debug:
+        for point in points:
             f.write("{} {} {}\n".format(point[0][0], point[1][0], point[2][0]))
 
 H_image_garment_file = "/home/def/Research/jresearch/2016-07-25-textiles-ironing/hoodie1/colored_mesh_1.ply-output.pcd-origin.txt"
@@ -88,6 +98,22 @@ H_board_kinfu_file = "/home/def/Research/jresearch/2016-07-25-textiles-ironing/h
 H_kinfu_cam_file = "/home/def/Research/jresearch/2016-07-25-textiles-ironing/hoodie1/0.txt"
 
 output_file = "/home/def/Research/jresearch/2016-07-25-textiles-ironing/hoodie1/traj.pcd"
+
+
+def send_trajectory_over_yarp(port, trajectory):
+    import yarp
+    yarp.Network.initMinimum()
+    p = yarp.Port()
+    p.open("/traj:o")
+    yarp.Network.connect("/traj:o", port)
+    trajectory_bottle = yarp.Bottle()
+    for point in trajectory:
+        b = trajectory_bottle.addList()
+        b.addDouble(point[0][0])
+        b.addDouble(point[1][0])
+        b.addDouble(point[2][0])
+    p.write(trajectory_bottle)
+
 
 if __name__ == '__main__':
     t = TrajectoryTransform()
@@ -98,5 +124,11 @@ if __name__ == '__main__':
     trajectory_cam = t(trajectory_image_px)
     trajectory_debug = t.debug(trajectory_image_px)
 
-    points_to_file(output_file)
+    points_to_file(trajectory_debug, output_file)
+
+    trajectory_root = t.relative_to_robot_root(trajectory_image_px)
+    print trajectory_root
+
+    send_trajectory_over_yarp("/read", trajectory_root)
+
 
