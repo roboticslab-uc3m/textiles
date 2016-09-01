54782618393602fc59dbcb8c9c26b8ef76bce690
David-Estevez <david.estevez.fdez@gmail.com>
2016-09-01 18:37:45 +0200

    Fix/adapt old code to work with new data

--- unfolding-industrial/apps/industrial-unfolding.py
+++ unfolding-industrial/apps/industrial-unfolding.py
@@ -6,9 +6,10 @@ import numpy as np
 from unfolding.perception.GarmentSegmentation import GarmentSegmentation
 from unfolding.perception.GarmentDepthMapClustering import GarmentDepthMapClustering
 from unfolding.perception.GarmentPickAndPlacePoints import GarmentPickAndPlacePoints
-import unfolding.perception.GarmentPlot
+from unfolding.perception import GarmentPlot
 
 path_input_mesh = "/home/def/Research/jresearch/2016-05-06-textiles-draft/pants1/textured_mesh.ply"
+#path_input_mesh = "/home/def/Research/jresearch/2016-04-20-textiles-draft/hoodie3/textured_mesh.ply"
 
 def sparse2dense(mask):
     """
@@ -17,7 +18,7 @@ def sparse2dense(mask):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-    # Maybe I don't need to fill the holes (?)
+    # Fill large holes
     mask_outlines, dummy = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.fillPoly(closing, mask_outlines, 255)
 
@@ -39,12 +40,12 @@ if __name__ == "__main__":
     # cloud depth data
     mask = sparse2dense(cv2.imread(path_mask, cv2.cv.CV_LOAD_IMAGE_GRAYSCALE))
     approximated_polygon = GarmentSegmentation.compute_approximated_polygon(mask)
-    unfolding.perception.GarmentPlot.plot_segmentation_stage(image_src, mask, approximated_polygon)
+    GarmentPlot.plot_segmentation_stage(image_src, mask, approximated_polygon)
 
     # Garment Depth Map Clustering Stage
-    preprocessed_depth_image = GarmentDepthMapClustering.preprocess(depth_image, mask)
-    labeled_image = GarmentDepthMapClustering.cluster_similar_regions(preprocessed_depth_image)
-    unfolding.perception.GarmentPlot.plot_clustering_stage(image_src, labeled_image)
+    preprocessed_depth_image = GarmentDepthMapClustering.preprocess_heightmap(depth_image, mask)
+    labeled_image = GarmentDepthMapClustering.cluster_similar_regions(preprocessed_depth_image, mask)
+    GarmentPlot.plot_clustering_stage(image_src, labeled_image)
 
     # Garment Pick and Place Points Stage
     unfold_paths = GarmentPickAndPlacePoints.calculate_unfold_paths(labeled_image, approximated_polygon)
@@ -54,5 +55,5 @@ if __name__ == "__main__":
     # GarmentPlot.plot_paths(image_src, approximated_polygon, unfold_paths)
     print "Bumpiness: ", bumpiness
     # GarmentPlot.plot_pick_and_place_points(image_src, pick_point, place_point)
-    unfolding.perception.GarmentPlot.plot_pick_and_place_stage(image_src, labeled_image, approximated_polygon, unfold_paths,
+    GarmentPlot.plot_pick_and_place_stage(image_src, labeled_image, approximated_polygon, unfold_paths,
                                                                pick_point, place_point)
