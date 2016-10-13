f94364be7a495d4887379527b9c9fa9c76e4dbc7
David-Estevez <david.estevez.fdez@gmail.com>
2016-10-13 15:45:48 +0200

    Add blank files for Li's algorithm

---
+++ ironing/perception/CurvatureScanLi.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+"""
+Curvature Scan Li
+---------------------------------------------------------------------
+Curvature scan as described in Yinxiao Li's Thesis and in article:
+ * Y. Li, X. Hu, D. Xu, Y. Yue, E. Grinspun, and P. Allen, “Multi-
+Sensor Surface Analysis for Robotic Ironing,” in IEEE International
+Conference on Robotics and Automation (ICRA), Stockholm, 2016.
+
+Finds height bumps on garments based on depth data.
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ ironing/perception/DiscontinuityScanLi.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+"""
+Discontinuity Scan Li
+---------------------------------------------------------------------
+Discontinuity scan as described in Yinxiao Li's Thesis and in article:
+ * Y. Li, X. Hu, D. Xu, Y. Yue, E. Grinspun, and P. Allen, “Multi-
+Sensor Surface Analysis for Robotic Ironing,” in IEEE International
+Conference on Robotics and Automation (ICRA), Stockholm, 2016.
+
+Finds wrinkles on garments based on rgb images taken with two different
+perpendicular illumination sources.
+"""
+
+if __name__ == '__main__':
+    pass
