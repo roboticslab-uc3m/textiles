6a918b4f4638e1646f8f5c4e8582009a3e3264dd
David-Estevez <david.estevez.fdez@gmail.com>
2016-12-19 15:09:38 +0100

    Refactor ROI functions to separate module

--- ironing/perception/DiscontinuityScanLi.py
+++ ironing/perception/DiscontinuityScanLi.py
@@ -11,6 +11,7 @@ import begin
 import logging
 from common.math import normalize_array
 from common.perception.Features import save_SIFT
+from common.perception.roi import load_roi_from_file, crop_roi
 
 """
 Discontinuity Scan Li
@@ -23,27 +24,6 @@ Conference on Robotics and Automation (ICRA), Stockholm, 2016.
 Finds wrinkles on garments based on rgb images taken with two different
 perpendicular illumination sources.
 """
-
-def load_roi_from_file(filename):
-    """
-    Loads region of interest from a file. The file format is the following:
-    start_x start_y
-    end_x end_y
-    :param filename: Path to the file
-    :return: a region of interest described as two points -> (start_x, start_y), (end_x, end_y)
-    """
-
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        (start_x, start_y), (end_x, end_y) = [map(int, line.split(' ')) for line in lines]
-
-    return (start_x, start_y), (end_x, end_y)
-
-def crop_roi(roi_rect, image):
-    """
-    Uses a ROI rectangle to crop a Region Of Interest of the image
-    """
-    return image[roi_rect[0][1]:roi_rect[1][1], roi_rect[0][0]:roi_rect[1][0]]
 
 class DiscontinuityScanLi(object):
     image_wrinkles_name_pattern = "garment-{:02d}-image-{:02d}.ppm"
