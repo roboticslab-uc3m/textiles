64203314c80d8e881b30aa329872736e374d4fb2
David.Estevez <david.estevez.fdez@gmail.com>
2017-01-06 01:07:43 +0100

    Delete old test code and add skeleton code for ScanLi class

--- ironing/perception/ScanLi.py
+++ ironing/perception/ScanLi.py
@@ -1,7 +1,20 @@
+# coding=utf-8
+
 import DiscontinuityScanLi
 import CurvatureScanLi
-import begin
 
-@begin.start
-def main():
-    print("Hello, Li!")
+"""
+Scan Li
+-----------------------------------------------------------------------
+Wrinkle detection using Discontinuity and Curvature scans, as described
+in Yinxiao Li's Thesis and in article:
+ * Y. Li, X. Hu, D. Xu, Y. Yue, E. Grinspun, and P. Allen, “Multi-
+Sensor Surface Analysis for Robotic Ironing,” in IEEE International
+Conference on Robotics and Automation (ICRA), Stockholm, 2016.
+
+Finds the most suitable paths for ironing depending on wrinkles
+(discontinuities) and height bumps.
+"""
+
+class ScanLi(object):
+    pass
