56d1f557e4e793560580f9f9542ca47662c513c5
David-Estevez <david.estevez.fdez@gmail.com>
2017-01-30 21:07:38 +0100

    Add wrapper for pcl code to perform garment segmentation

--- unfolding_industrial/perception/GarmentDepthSegmentation.py
+++ unfolding_industrial/perception/GarmentDepthSegmentation.py
@@ -1,18 +1,31 @@
-import cv2
-import numpy as np
+import os
+import subprocess
+from skimage.io import imread
 
 from unfolding.perception.GarmentSegmentation import GarmentSegmentation
 
+pcl_processing_binary = "foldingClothesMesh"
+pcl_processing_folder = "~/Repositories/textiles/pcl/build"
+
+
 class GarmentDepthSegmentation(GarmentSegmentation):
     @staticmethod
-    def background_subtraction(image):
+    def background_subtraction(image: 'Point Cloud filepath'):
         """
         Segments the garment from the background. This implementation takes a point cloud
         and uses RANSAC to segment it from the table
         :param image: Point Cloud filepath
         :return: Segmentation mask where white is garment and black is background
         """
+        # Process input 'image'
+        args = [os.path.expanduser(os.path.join(pcl_processing_folder, pcl_processing_binary)),
+                "--ransac-threshold",
+                str(0.0035),
+                image]
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
 
+        # Load generated mask
+        mask = imread(image+'-mask.png')
 
-
-        return filtered_mask_open
+        return mask
